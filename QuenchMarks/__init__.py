fce8257b795608f1684925c23a456ac9055d5fc0
Jason Serafica <seraficaj@gmail.com>
2021-11-17 19:08:01 -0800

    begin restructuring app to incorporate separated routes and begin work on auth

--- QuenchMarks/__init__.py
+++ QuenchMarks/__init__.py
@@ -1,32 +1,24 @@
-import os
+import os 
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
-from flask import Flask
+app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'topsecretlol'
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
+### Database Setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "data.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+db = SQLAlchemy(app)
+Migrate(app, db)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+@app.route('/')
+def index(): 
+    return render_template('home.html')
 
-    # a simple page that says hello
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
-    return app
--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template
+print('hello world ~ app.py!')
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
+from logging import debug
+from QuenchMarks import app
 if __name__ == '__main__':
     app.run(debug=True)
