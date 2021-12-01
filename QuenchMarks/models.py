eac78d532cf1dd441efc756ec7f8f8de83ecffdf
Jason Serafica <seraficaj@gmail.com>
2021-12-01 09:33:11 -0800

    begin work on bottles model

--- QuenchMarks/models.py
+++ QuenchMarks/models.py
@@ -27,3 +27,11 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"Username {self.username}"
+
+
+class Bottle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(64), nullable=False)
+    material = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    volume = db.Column(db.Integer)
