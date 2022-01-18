337752f652e75ad78cbfaf8f55d34ae9676f783d
Jason Serafica <seraficaj@gmail.com>
2022-01-18 09:57:19 -0800

    create user favorites table in models.py

--- QuenchMarks/models.py
+++ QuenchMarks/models.py
@@ -8,6 +8,10 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(user_id)
 
+user_favorites = db.Table('favorites', 
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('bottle_id', db.Integer, db.ForeignKey('bottle.id'), primary_key=True)
+)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +19,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     reviews = db.relationship('Review', backref='user', lazy=True)
+    favorites = db.relationship('Bottle', secondary=user_favorites, backref='favorited_by')
 
     def __init__(self, email, username, password):
         self.email = email
