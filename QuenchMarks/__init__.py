5ba483909783b0d199e23b6901546565c9fff043
Jason Serafica <seraficaj@gmail.com>
2022-02-13 19:26:54 -0800

    swtich db to postgres

--- QuenchMarks/__init__.py
+++ QuenchMarks/__init__.py
@@ -10,9 +10,7 @@ app.config['SECRET_KEY'] = 'topsecretlol'
 
 ### Database Setup
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    basedir, "data.sqlite"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///quenchmarks"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
---
+++ migrations/versions/f490ac8ca801a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f490ac8ca801a
+Revises: 
+Create Date: 2022-02-13 19:23:55.536399
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f490ac8ca801a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
