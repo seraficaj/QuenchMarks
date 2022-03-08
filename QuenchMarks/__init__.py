e59055b41a69da1ec3eefdfd97e914a137a71724
Jason Serafica <seraficaj@gmail.com>
2022-03-07 19:35:49 -0800

    confirm app works with python-dotenv

--- QuenchMarks/__init__.py
+++ QuenchMarks/__init__.py
@@ -1,16 +1,24 @@
 import os 
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+### import .env
+load_dotenv()
+
+DB_URI = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY");
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'topsecretlol'
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 ### Database Setup
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///quenchmarks"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
