eafc2336c9c57f83317fbcf7a095a83103b9ac78
Jason Serafica <seraficaj@gmail.com>
2023-09-16 14:30:32 -0700

    update bottle model with posted by field

--- QuenchMarks/models.py
+++ QuenchMarks/models.py
@@ -8,18 +8,23 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(user_id)
 
-user_favorites = db.Table('favorites', 
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('bottle_id', db.Integer, db.ForeignKey('bottle.id'), primary_key=True)
+
+user_favorites = db.Table(
+    "favorites",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("bottle_id", db.Integer, db.ForeignKey("bottle.id"), primary_key=True),
 )
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    reviews = db.relationship('Review', backref='user', lazy=True)
-    favorites = db.relationship('Bottle', secondary=user_favorites, backref='favorited_by')
+    reviews = db.relationship("Review", backref="user", lazy=True)
+    favorites = db.relationship(
+        "Bottle", secondary=user_favorites, backref="favorited_by"
+    )
 
     def __init__(self, email, username, password):
         self.email = email
@@ -39,7 +44,10 @@ class Bottle(db.Model):
     brand = db.Column(db.String(64), nullable=False)
     material = db.Column(db.String(64), nullable=False)
     volume = db.Column(db.Integer)
-    reviews = db.relationship('Review', cascade="all,delete", backref='bottle', lazy=True)
+    reviews = db.relationship(
+        "Review", cascade="all,delete", backref="bottle", lazy=True
+    )
+    posted_by = db.Column(db.String(64))
 
 
 class Review(db.Model):
